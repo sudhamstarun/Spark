265c75ac13fb0cea743b7a13ff0bfdb6f4dc18b2
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-02-21 00:35:30 +0800

    import all kits and add minor extension

---
+++ Measure/Spark/Spark/General/Category/UIView+Extension.swift
@@ -0,0 +1,45 @@
+//
+//  UIView+Extension.swift
+//  Spark
+//
+//  Created by Tarun Sudhams on 21/2/2019.
+//  Copyright © 2019 Tarun Sudhams. All rights reserved.
+//
+
+import UIKit
+
+enum InsideLayout {
+    static let `default`: [InsideLayout] = [.top, .bottom, .left, .right]
+    case top
+    case bottom
+    case left
+    case right
+}
+
+public protocol Then {}
+extension Then where Self: AnyObject {
+    public func then(_ block: (Self) -> Void) -> Self {
+        block(self)
+        return self
+    }
+}
+
+extension NSObject: Then {}
+
+extension UIView {
+    func layoutInside(view: UIView, inset: UIEdgeInsets, options: [InsideLayout] = InsideLayout.default) {
+        if options.contains(.top) {
+            self.topAnchor.constraint(equalToSystemSpacingBelow: view.topAnchor, multiplier: inset.top)
+        }
+        if options.contains(.bottom) {
+            self.bottomAnchor.constraint(equalToSystemSpacingBelow: view.bottomAnchor, multiplier: inset.bottom)
+        }
+        if options.contains(.left) {
+            self.leftAnchor.constraint(equalToSystemSpacingAfter: view.leftAnchor, multiplier: inset.left)
+        }
+        if options.contains(.right) {
+            self.rightAnchor.constraint(equalToSystemSpacingAfter: view.rightAnchor, multiplier: inset.right)
+        }
+    }
+}
+
