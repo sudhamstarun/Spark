40df28a621722301a397dfe76ca6349a93873d5a
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-02-21 23:58:53 +0800

    switch to remix IDE

---
+++ Blockchain/Smart Contracts/mortgage.sol
@@ -0,0 +1,8 @@
+pragma solifity ^0.4.4;
+
+contract mortgage{
+    /* constructor will be used to deploy the smart contract on blockchain.
+    Setting initial account balance to 1000000 and loan status to 'Initiated;.
+    */
+
+}
