c36936270449f5ddbda4e7703d934f10a83fce67
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-02-21 22:47:34 +0800

    migrate done

--- Blockchain/Smart Contracts/migrate.sol
+++ Blockchain/Smart Contracts/migrate.sol
@@ -11,4 +11,13 @@ contract migrate{
     function migrate(){
         owner = msg.sender;
     }
+
+    function setCompleted(uint completed) restricred {
+        last_completed_migration = completed;
+    }
+
+    function upgrade(address new_address) restricred{
+        migrate upgraded = migrate(new_address);
+        upgraded.setCompleted(last_completed_migration)
+    }
 }
