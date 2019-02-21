680ebd6a228922db09afa5651c5eecedcd8d59ce
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-02-22 00:52:45 +0800

    mapping addresses

--- Blockchain/Smart Contracts/mortgage.sol
+++ Blockchain/Smart Contracts/mortgage.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.1;
+pragma solidity ^0.5.4;
 
 contract mortgage{
     /* constructor will be used to deploy the smart contract on blockchain.
@@ -7,12 +7,88 @@ contract mortgage{
     have to stick with float.
     */
 
+    // Declaring all the necessary
+    int constant STATUS_INITIATED = 0;
+    int constant STATUS_SUBMITTED = 1;
+    int constant STATUS_APPROVED  = 2;
+    int constant STATUS_REJECTED  = 3;
+
+    // store the property details
+    struct Property {
+      bytes32  addressOfProperty;
+      uint32 purchasePrice;
+      address owner;
+    }
+
+    // store the loan terms
+    struct LoanTerms{
+      uint32 term;
+      uint32 interest;
+      uint32 loanAmount;
+      uint32 annualTax;
+      uint32 annualInsurance;
+    }
+
+    // struct datatype to store the monthly payment structure
+    struct MonthlyPayment{
+        uint32 pi;
+        uint32 tax;
+        uint32 insurance;
+    }
+
+    // store the details of the loan contract */
+    struct Loan {
+        LoanTerms loanTerms;
+        Property property;
+        MonthlyPayment monthlyPayment;
+        ActorAccounts actorAccounts;
+        int status; // values: REJECTED, SUBMITTED, APPROVED,
+    }
+
+
+    // names of the nodes that would be present on the blockchain
+    struct ActorAccounts {
+      address mortgageHolder;
+      address insurer;
+      address irs;
+    }
+
+    // name instances of our structs
+    Loan loan;
+    LoanTerms loanTerms;
+    Property property;
+    MonthlyPayment monthlyPayment;
+    ActorAccounts actorAccounts;
+
+    //FUNCTIONS DEFINED BELOW
+
     function Mortgage() public{
         loanApplicant = msg.sender;
+        loan.status = STATUS_INITIATED;
+        balances[msg.sender] = 100000000000000000000000;
     }
 
     // address of the loanApplicant
-
     address loanApplicant;
 
+
+    // Events - publicize actions to external listeners
+    event LienReleased(address _owner);
+    event LienTrasferred (address _owner);
+    event LoanStatus (int _status);
+
+    // CONDUCTING MAPPING now
+
+   // Maps addresses of the actors in the mortgage contract with their balances
+
+   mapping (address => uint256) public balances;
+
+   /* Function called by mortgageHolder */
+
+    modifier bankOnly {
+      if(msg.sender != loan.actorAccounts.mortgageHolder) {
+         revert("lol");
+      }
+      _;
+   }
 }
