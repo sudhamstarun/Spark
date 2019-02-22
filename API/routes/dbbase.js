057abf9de59edf7c93b79b422b67d3d2f23f695d
atibrewal98 <ankit@siplnet.com>
2019-02-23 02:53:32 +0800

    Clean File

--- API/routes/dbbase.js
+++ API/routes/dbbase.js
@@ -1,8 +1,6 @@
 'use strict'
 
 var sql = require('mssql');
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request;
 
 var dbConfig = {
     user: "sparkadmin",
@@ -15,58 +13,6 @@ var dbConfig = {
         encrypt: true
     }
   };
-
-  //var connection = new Connection(dbConfig);
-  
-//   exports.executequery = function (req, res, query) {
-//     connection.on('connect', function (err) {
-//       if (err) {
-//         console.log("Error while connecting database :- " + err);
-//         res.send(err);
-//       }
-//       else {
-//         console.log('Connection Done!');
-  
-//         // create Request object
-//         var request = new Request(query, function(err, result){
-//             if(err){
-//                 console.log("Error while querying database :- " + err);
-//                 res.send(err);
-//                 process.exit();
-//             }
-//             else{
-//                 res.send(result);
-//                 process.exit();
-//             }
-//         });
-//         connection.execSql(request);
-//       }
-//     });
-//   }
-
-//   exports.executequery = function (req, res, query) {
-//     connection.on('connect', function (err) {
-//       if (err) {
-//         console.log("Error while connecting database :- " + err);
-//         res.send(err);
-//       }
-//       else {
-//         console.log('Connection Done!');
-  
-//         // create Request object
-//         var request = new Request(query, function(err, rowCount, rows){
-//             console.log(rowCount + ' row(s) returned');
-//             process.exit();
-//         });
-//         request.on('row', function(columns) {
-//             columns.forEach(function(column) {
-//                 console.log("%s\t%s", column.metadata.colName, column.value);
-//             });
-//         });
-//         connection.execSql(request);
-//       }
-//     });
-//   }
 
 exports.executequery = function (req, res, query) {
     sql.connect(dbConfig, function (err) {
