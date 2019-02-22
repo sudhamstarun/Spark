6b854dae1efb67d0427056371b431223fe2db945
atibrewal98 <ankit@siplnet.com>
2019-02-23 00:32:12 +0800

    Update Libs, setup ports

--- API/index.js
+++ API/index.js
@@ -4,3 +4,21 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var http = require('http');
 var https = require('https');
+var sql = require('mssql');
+
+var app = express();
+
+//BODY Parser Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Setup CORS Middleware
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+    next();
+  });
+
+app.set('port', process.env.PORT || 8081);
+
