2711d4479420dcae4a6a9e54c200d4be1512fe7e
atibrewal98 <ankit@siplnet.com>
2019-02-23 02:12:24 +0800

    Query SQL

--- API/routes/dbbase.js
+++ API/routes/dbbase.js
@@ -1,6 +1,8 @@
 'use strict'
 
 var sql = require('mssql');
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
 
 var dbConfig = {
     user: "sparkadmin",
@@ -12,9 +14,10 @@ var dbConfig = {
         encrypt: true
     }
   };
+  var connection = new Connection(dbConfig);
   
   exports.executequery = function (req, res, query) {
-    sql.connect(dbConfig, function (err) {
+    connection.on('connect', function (err) {
       if (err) {
         console.log("Error while connecting database :- " + err);
         res.send(err);
@@ -23,21 +26,18 @@ var dbConfig = {
         console.log('Connection Done!');
   
         // create Request object
-        var request = new sql.Request();
-  
-        // query to the database
-        request.query(query, function (err, result) {
-          if (err) {
-            console.log("Error while querying database :- " + err);
-            res.send(err);
-            sql.close();
-          }
-          else {
-            //console.log(result);
-            res.send(result);
-            sql.close();
-          }
+        var request = new Request(query, function(err, result){
+            if(err){
+                console.log("Error while querying database :- " + err);
+                res.send(err);
+                process.exit();
+            }
+            else{
+                res.send(result);
+                process.exit();
+            }
         });
+        connection.execSql(request);
       }
     });
   }
