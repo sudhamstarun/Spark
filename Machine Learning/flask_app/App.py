7b308a16ad904c4726b81f7309e9318c963d4095
Rajat Jain <jain.rajat50@gmail.com>
2019-02-24 09:24:39 +0800

    finalized flask app

--- Machine Learning/flask_app/App.py
+++ Machine Learning/flask_app/App.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import numpy as np
-from keras.models import model_from_json
+from keras.models import load_model
+import json
 
 app = Flask(__name__)
 
@@ -9,23 +10,31 @@ app = Flask(__name__)
 def Hello():
     return "Server Up"
 
-@app.route('/risk' methods=['POST'])
+@app.route('/risk', methods=['POST'])
 def riskCalculate():
-    #Loading Model
-    json_file = open('final_model.json', 'r')
-    model_json = json_file.read()
-    json_file.close()
-
-    model = model_from_json(model_json)
-
-    model.load_weights('final_weights.hdf5')
-
-    model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
 
     #Handling JSON Data from Post Request
+    data = request.json
 
+    input_data = np.zeros((1,7))
 
-    #Calculating Delinquicy
-    pred = model.predict()
+    input_data[0][0] = data["LAST_RT"]
+    input_data[0][1] = data["LAST_UPB"]
+    input_data[0][2] = data["Loan.Age"]
+    input_data[0][3] = data["Months.To.Legal.Mat"]
+    input_data[0][4] = data["Adj.Month.To.Mat"]
+    input_data[0][5] = data["MSA"]
+    input_data[0][6] = data["Maturity.date"]
+
+    #Loading Model
+    model = load_model('final_model.hdf5')
+
+    #Making the prediction
+    pred = float(model.predict(input_data))
+    pred = round(pred)
+    if pred == 1:
+        outcome= "Default Predicted"
+    else:
+        outcome = "Will be able to repay"
+    result = { 'predicted_outcome' : outcome }
+    return json.dumps(result)
